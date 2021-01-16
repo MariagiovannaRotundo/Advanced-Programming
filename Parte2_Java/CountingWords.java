ba21c0a7f9bd750712d034fcac0c6667aefccf88
Mary <mariagiovannarotundo@gmail.com>
2021-01-16 02:43:03 +0100

    Update CountingWords.java

--- Parte2_Java/CountingWords.java
+++ Parte2_Java/CountingWords.java
@@ -17,6 +17,9 @@ public class CountingWords extends MapReduce<String, String, List<String>, Integ
 	private Path path;
 	File file_write;
 	
+	
+	//costructor. ask the user to insert the path of the directory where
+	//files are
 	public CountingWords(String namefile){
 		Scanner s = new Scanner(System.in);
 	    String string_path= s.nextLine();
@@ -45,28 +48,30 @@ public class CountingWords extends MapReduce<String, String, List<String>, Integ
 		elements.forEach(
 				//for each file
 				x -> {
-					//get list of strings
+					//get list of strings of the file
 					List<String> list = x.getValue();
 					
-					//for each string/line
+					//for each string/line isolate the words
 					for(String s : list){
 						String curr_s = s.toLowerCase().replaceAll("[^a-z0-9]", " ");
 						String[] words = curr_s.split(" ");
-						
+						//sort the words
 						Arrays.sort(words);
-						
+						//if there are words
 						if(words.length>0){
 				
 							String currword = words[0];
 							int occ=1;
 							
-							//for each word
+							//for each word count the occurrences and
+							//if its length is greater of 3 add to list 
+							//for the stream.
 							for(int i=1; i<words.length; i++){
-								
+								//an occurrence is found
 								if(words[i].compareTo(currword)==0){
 									occ++;
 								}
-								else{
+								else{//another word is found
 									if(currword.length()>3){
 										newList.add(new Pair<>(currword, occ));
 									}
@@ -100,15 +105,16 @@ public class CountingWords extends MapReduce<String, String, List<String>, Integ
 	protected Stream<Pair<String, Integer>> reduce(Stream<Pair<String, List<Integer>>> elements) {
 		
 		List<Pair<String,Integer>> newList = new ArrayList<>();
-		
+		//for each element (w, list number occurrences)
 		elements.forEach(
 			x -> {
+					//sum the occurrences in the list of the word
 					int sum=0;
 					List<Integer> list = x.getValue();
 					for(Integer element : list){
 		                sum += element;
 		            }
-					
+					//add the pair (word, number occurrence)
 					newList.add(new Pair<>(x.getKey(), sum));
 				}
 		);
@@ -123,6 +129,7 @@ public class CountingWords extends MapReduce<String, String, List<String>, Integ
 	@Override
 	protected void write(Stream<Pair<String, Integer>> output) {
 		try {
+			//write the obtained stream in the indicated file
 			Writer.write(this.file_write, output);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
