2b32d75e32286b78f915c9c45be58b89c25ef556
Mary <mariagiovannarotundo@gmail.com>
2021-01-16 23:42:07 +0100

    Create MainInverted.java

---
+++ Parte2_Java/MainInverted.java
@@ -0,0 +1,12 @@
+package myMapReduce;
+
+public class MainInverted {
+
+	public static void main(String[] args) {
+		
+		//to test invertedIndex execution
+		InvertedIndex i= new InvertedIndex("output_inverted");
+		i.start();
+
+	}
+}
