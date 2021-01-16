93bf714c7745f089c89d72d0fd0b305dd9749d14
Mary <mariagiovannarotundo@gmail.com>
2021-01-16 02:43:07 +0100

    Update Main.java

--- Parte2_Java/Main.java
+++ Parte2_Java/Main.java
@@ -4,9 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//to Count words into files
 		CountingWords c= new CountingWords("output_counting");
 		c.start();
 		
+		//to test invertedIndex execution
 		//InvertedIndex i= new InvertedIndex("output_inverted");
 		//i.start();
 
